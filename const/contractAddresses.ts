5ac0b7b98557b2ed63a17786db86d5b23c3a193b
Rismandev <68722801+rismandev96@users.noreply.github.com>
2023-06-17 19:32:24 +0700

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -25,5 +25,5 @@ export const NETWORK: CustomChain = {
 
 export const MARKETPLACE_ADDRESS = "0x9D71f10Df7387C35756aE7a3f56B178Fa42D6fAf";
 export const NFT_COLLECTION_ADDRESS =
-  "0xFfd9bAddF3f6e427EfAa1A4AEC99131078C1d683";
+  "0x2c47544a8cDCc089E851e7BC8A6Cbfc05d96229B";
 export const ETHERSCAN_URL = "https://explorer.omtch.com/";
