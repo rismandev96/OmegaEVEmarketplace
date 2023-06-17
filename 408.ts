9155b44be0b8c0b5cfb568fa94d60db544840a07
Rismandev <68722801+rismandev96@users.noreply.github.com>
2023-06-17 18:31:05 +0700

    Add files via upload

---
+++ 408.ts
@@ -0,0 +1,20 @@
+import type { Chain } from "../src/types";
+export default {
+  "name": "Omega Main Network",
+  "chain": "OmegaMainNetwork",
+  "rpc": [
+    "https://mainnet-rpc.omtch.com/"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "OmegaMainNetwork",
+    "symbol": "OMN",
+    "decimals": 18
+  },
+  "infoURL": "https://explorer.omtch.com/",
+  "shortName": "OMN",
+  "chainId": 408,
+  "networkId": 408,
+  "testnet": false,
+  "slug": "omega-main-network"
+} as const satisfies Chain;
