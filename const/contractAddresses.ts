78000a4e23997635f9f48564857a68853e829d3e
Rismandev <68722801+rismandev96@users.noreply.github.com>
2023-06-17 18:09:07 +0700

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,7 +2,7 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
+import { OmegaMainNetwork } from "@thirdweb-dev/chains";
 export const NETWORK = OmegaMainNetwork;
 
 // 2. The address of the marketplace V3 smart contract.
