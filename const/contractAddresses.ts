3b8ca16ee77f2ecd658729e76e969b6e2831aace
Rismandev <68722801+rismandev96@users.noreply.github.com>
2023-06-18 07:58:49 +0700

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -23,6 +23,12 @@ export const NETWORK: CustomChain = {
   slug: "omega-main-network",
 };
 
+export const nftDropContractAddress =
+  "0x2c47544a8cDCc089E851e7BC8A6Cbfc05d96229B";
+export const tokenContractAddress =
+  "0xCaC4007aafd9708193F4d376167EE4f2ec8e1fe7";
+export const stakingContractAddress =
+  "0xeB9c7C39077b6E3881118b9768AAB07c1ebA0045";
 export const MARKETPLACE_ADDRESS = "0x9D71f10Df7387C35756aE7a3f56B178Fa42D6fAf";
 export const NFT_COLLECTION_ADDRESS =
   "0x2c47544a8cDCc089E851e7BC8A6Cbfc05d96229B";
